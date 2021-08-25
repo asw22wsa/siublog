ea39819811a6ecd73de7a435564a62ad6438730d
asw11wsa <siudevelop@gmail.com>
2021-08-25 22:19:25 +0900

    change StudyController path and add 'use App\Http\Controllers\Controller;' then change api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudyController;
+use App\Http\Controllers\Api\StudyController;
 
 /*
 |--------------------------------------------------------------------------
