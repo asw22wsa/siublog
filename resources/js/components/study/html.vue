19a0498bba99eea61750f257d8618a80f04b06f5
asw11wsa <asw11wsa@naver.com>
2021-08-25 14:18:57 +0900

    i writer basic vue code and make file named header,footer

---
+++ resources/js/components/study/html.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+export default {
+  name:'',
+  components : {},
+  data(){
+    return {
+      sampleData:''
+    };
+  },
+  setup(){},
+  created(){},
+  mounted(){},
+  methods: {
+  },
+}
+</script>
+<style scoped>
+
+</style>
