ccbb83ab6e62818d22f550e0d2965a53d22ec165
asw11wsa <siudevelop@gmail.com>
2021-09-02 10:59:52 +0900

    import bootstrap-vue in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,11 +3,14 @@ import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import store from './store'
 
 new Vue({
     el: '#app',
     router,
     store,
+    BootstrapVue,
+    IconsPlugin,
     render: h => h(App)
 });
